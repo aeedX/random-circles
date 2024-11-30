41d90a4c0f66f5f42c2f5bf8e1d09d5c7f5d5b25
keinerex <Yurkov22Vl@gmail.com>
2024-11-30 17:20:11 +0700

    commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
